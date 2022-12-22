6c582b3fd20631430adb35eb82fda9be48ca40c8
root <sabinorsp@gmail.com>
2022-12-22 17:29:43 -0300

    att2

--- app/ui.R
+++ app/ui.R
@@ -4,7 +4,7 @@ source('functions/plot_results.R')
 
 ui <- fluidPage(
   
-  titlePanel("Histórico Notas Residencia Médica PSU 2022 - MG "),
+  titlePanel("Histórico Notas Residência Médica PSU 2022 - MG "),
   h5('Essa aplicação reúne os resultados das notas da edição do PSU de 2022-MG.'),
   h5('Esse Dashboard tem como objetivo  promover  pesquisas em dados históricos dos resultados 
      e servir ao candidato uma visualização de forma direta e simplificada.'),
@@ -19,7 +19,7 @@ ui <- fluidPage(
       h5('Desejamos continuar evoluindo com esta aplicação! 
          Por isso gostariamos de receber comentários e críticas! '),
       h5('Caso deseje contribuir, basta encaminhar um email com assunto FEEDBACK para:'),
-      tags$a(href="https://forms.gle/vMN9jdCBhrfTtjqL9", "sabinorsp@gmail.com")
+      tags$a(href="https://forms.gle/vMN9jdCBhrfTtjqL9", "Acesso Formulário.")
       ),# End sidebarPanel
       
     mainPanel(
